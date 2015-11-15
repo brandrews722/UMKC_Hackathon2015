d6505fec0ad6478ec7ee0908f8c3b9aa8c57940a
bcap68 <bcap68@mail.umkc.edu>
2015-11-14 18:05:20 -0600

    added date and time input

--- Src/doraa/www/js/controllers.js
+++ Src/doraa/www/js/controllers.js
@@ -11,93 +11,92 @@ appController.controller('signUpController', function ($scope, $state) {
 })
 
 appController.controller('weatherCtrl', function ($scope, $state, $http, weatherFactory, latLngFactory) {
-        var sourcePlace;
-        var destinationPlace;
-        var autocompleteForSource;
-        var autoCompleteForDestination;
-        $scope.init = function () {
-            var mapOptions = {
-                zoom: 6,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-            $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var sourcePlace;
+    var destinationPlace;
+    var autocompleteForSource;
+    var autoCompleteForDestination;
+    $scope.init = function () {
+        var mapOptions = {
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-            var sourceInput = document.getElementById('txt_SourcePlace');
-            autocompleteForSource = new google.maps.places.Autocomplete(sourceInput);
-            google.maps.event.addListener(autocompleteForSource, 'place_changed', function () {
-                sourcePlace = autocompleteForSource.getPlace();
-            });
-            var destInput = document.getElementById('txt_DestinationPlace');
-            autoCompleteForDestination = new google.maps.places.Autocomplete(destInput);
-            google.maps.event.addListener(autoCompleteForDestination, 'place_changed', function () {
-                destinationPlace = autoCompleteForDestination.getPlace();
-            });
-            //This piece of code was referred from Google Maps API docs. https://developers.google.com/maps/articles/geolocation?hl=en
-            if (navigator.geolocation) {
-                browserSupportFlag = true;
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                    $scope.map.setCenter(initialLocation);
-                }, function () {
-                    handleNoGeolocation(browserSupportFlag);
-                });
-            }
-            // Browser doesn't support Geolocation
-            else {
-                browserSupportFlag = false;
-                handleNoGeolocation(browserSupportFlag);
-            }
-
-            function handleNoGeolocation(errorFlag) {
-                //Failure of the service.##
-                if (errorFlag == true) {
-                    alert("Geolocation service failed.");
-                    initialLocation = newyork;
-                } else {
-                    alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-                    initialLocation = siberia;
-                }
+        var sourceInput = document.getElementById('txt_SourcePlace');
+        autocompleteForSource = new google.maps.places.Autocomplete(sourceInput);
+        google.maps.event.addListener(autocompleteForSource, 'place_changed', function () {
+            sourcePlace = autocompleteForSource.getPlace();
+        });
+        var destInput = document.getElementById('txt_DestinationPlace');
+        autoCompleteForDestination = new google.maps.places.Autocomplete(destInput);
+        google.maps.event.addListener(autoCompleteForDestination, 'place_changed', function () {
+            destinationPlace = autoCompleteForDestination.getPlace();
+        });
+        //This piece of code was referred from Google Maps API docs. https://developers.google.com/maps/articles/geolocation?hl=en
+        if (navigator.geolocation) {
+            browserSupportFlag = true;
+            navigator.geolocation.getCurrentPosition(function (position) {
+                initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                 $scope.map.setCenter(initialLocation);
-            }
+            }, function () {
+                handleNoGeolocation(browserSupportFlag);
+            });
+        }
+        // Browser doesn't support Geolocation
+        else {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
         }
 
-
-
-        //function to load directions and get weather events for the start and end points
-        $scope.doEverything = function () {
-
-            function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-                directionsService.route({
-                    origin: document.getElementById('txt_SourcePlace').value,
-                    destination: document.getElementById('txt_DestinationPlace').value,
-                    travelMode: google.maps.TravelMode.DRIVING
-                }, function (response, status) {
-                    if (status === google.maps.DirectionsStatus.OK) {
-                        directionsDisplay.setDirections(response);
-                    } else {
-                        window.alert('Directions request failed due to ' + status);
-                    }
-                });
+        function handleNoGeolocation(errorFlag) {
+            //Failure of the service.##
+            if (errorFlag == true) {
+                alert("Geolocation service failed.");
+                initialLocation = newyork;
+            } else {
+                alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+                initialLocation = siberia;
             }
-            console.log(document.getElementById('txt_SourcePlace').value);
+            $scope.map.setCenter(initialLocation);
+        }
+    }
 
-            var directionsService = new google.maps.DirectionsService;
-            var directionsDisplay = new google.maps.DirectionsRenderer;
 
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 12,
-                center: {
-                    lat: 41.85,
-                    lng: -87.65
+
+    //function to load directions and get weather events for the start and end points
+    $scope.doEverything = function () {
+
+        function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+            directionsService.route({
+                origin: document.getElementById('txt_SourcePlace').value,
+                destination: document.getElementById('txt_DestinationPlace').value,
+                travelMode: google.maps.TravelMode.DRIVING
+            }, function (response, status) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
+                } else {
+                    window.alert('Directions request failed due to ' + status);
                 }
             });
-            directionsDisplay.setMap(map);
-            calculateAndDisplayRoute(directionsService, directionsDisplay);
-            weatherFactory.getWeatherStart();
-            weatherFactory.getWeatherEnd();
-            document.getElementById("weatherIncrements").innerHTML = "<img id='elNino' src='http://i.kinja-img.com/gawker-media/image/upload/s--tI4ZDhBj--/na2jhagaer4erqt9vqqj.gif'>";
         }
-    })
+
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: {
+                lat: 41.85,
+                lng: -87.65
+            }
+        });
+        directionsDisplay.setMap(map);
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+        weatherFactory.getWeatherStart();
+        weatherFactory.getWeatherEnd();
+        document.getElementById("weatherIncrements").innerHTML = "<img id='elNino' src='http://i.kinja-img.com/gawker-media/image/upload/s--tI4ZDhBj--/na2jhagaer4erqt9vqqj.gif'>";
+    }
+})
 
 
 appController.factory('weatherFactory', function ($http, latLngFactory) {
@@ -171,6 +170,9 @@ appController.factory('weatherFactory', function ($http, latLngFactory) {
                     })
                 }
             })
+        },
+        getWeatherMiddle: function () {
+
         }
     }
 })
